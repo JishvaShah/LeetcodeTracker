b44f2874e53e8decf3dd7f8ba2b996d45f77103c
JishvaShah <77269630+JishvaShah@users.noreply.github.com>
2023-12-28 11:54:17 -0800

    Time: 24 ms (51.87%), Space: 46.9 MB (25.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,20 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-       int sum=0,count=0,len=0,maxLen=0;
-        Map<Integer, Integer> prefixSumMap = new HashMap<>();
+       int sum=0,count=0;
+        HashMap<Integer, Integer> prefixSumMap = new HashMap<>();
         
         for(int i=0;i<nums.length;i++){
             //calculating prefix sum at each index
             sum+=nums[i];
             //checking if prefixsum is already equals to k
             if(sum==k) count++;
-            //calculating remainder sum - k
-            int rem = sum - k;
+            //calculating remainder sum - k and then
             //checking if remainder is present in map, to check occurence
             //if yes add it to count
-            if(prefixSumMap.containsKey(rem)) count+=prefixSumMap.get(rem);
+            if(prefixSumMap.containsKey(sum - k)) count+=prefixSumMap.get(sum - k);
             //finally, check if prefixSum is present in the map or not
-            //if present, get its key value and incremenet it by 1(incrementing occurence by 1)
+            //if present, get its key value and incremenet it by 1
+            //(incrementing occurence by 1)
             //if not present, simply add it to map with value 1 as its occurence count
             if(!prefixSumMap.containsKey(sum)) prefixSumMap.put(sum, 1);
             else{
