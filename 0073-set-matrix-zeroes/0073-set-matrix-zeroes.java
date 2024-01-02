b471474822875c3e7e79031186586f32ef753998
JishvaShah <77269630+JishvaShah@users.noreply.github.com>
2024-01-02 14:52:11 -0800

    Time: 1 ms (90.40%), Space: 45.5 MB (16.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,22 +1,42 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] row = new int[matrix.length];
-        int[] col = new int[matrix[0].length];
-            
+        int row = matrix.length;
+        //row=matrix[..][0]
+        int col = matrix[0].length;
+        //col=matrix[0][..]
+        
+        int col0=1;
         //first traversal
-        for(int i=0;i<row.length;i++){
-            for(int j=0;j<col.length;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    matrix[i][0]=0;
+                    
+                    if(j!=0)
+                        matrix[0][j]=0;
+                    else 
+                        col0=0;
                 }
             }
         }
         
-        for(int i=0;i<row.length;i++){
-            for(int j=0;j<col.length;j++){
-                if(row[i]==1 || col[j]==1) matrix[i][j]=0;
+        //second traversal except first row and first col
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+                }
             }
         }
+        
+        
+        //third traversal for first row
+        if(matrix[0][0]==0) 
+            for(int i=0;i<col;i++) matrix[0][i]=0;
+        //fourth traversal for first column
+        if(col0==0)
+            for(int i=0;i<row;i++) matrix[i][0]=0;
+
+        
     }
 }
