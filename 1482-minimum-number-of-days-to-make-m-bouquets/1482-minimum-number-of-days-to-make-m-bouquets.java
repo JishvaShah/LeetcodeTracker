39adf660523a750d4484a6349990a88032c4e35c
JishvaShah <77269630+JishvaShah@users.noreply.github.com>
2024-01-22 15:27:13 -0800

    Time: 21 ms (44.87%), Space: 57.7 MB (52.26%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public static boolean isValid(int[] bloomDay, int m, int k, int mid){
+        int totalCount=0/*m*/,count=0/*k*/;
+        for(int i=0;i<bloomDay.length;i++){
+            if(bloomDay[i]<=mid) count++;
+            else count=0;
+            if(count==k) {
+                totalCount++;
+                count=0;
+            }
+        }
+        return totalCount>=m;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        int low=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<bloomDay.length;i++){
+            low=Math.min(low,bloomDay[i]);
+            max=Math.max(max,bloomDay[i]);
+        }
+        int high=max; 
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isValid(bloomDay,m,k,mid)) high=mid-1;
+            else low=mid+1;
+        }
+        
+        return low==(max+1)?-1:low;   
+    }
+}
