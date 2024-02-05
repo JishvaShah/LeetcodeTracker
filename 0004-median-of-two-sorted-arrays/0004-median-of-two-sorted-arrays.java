d198552da5f8d65c9d408015d19911c4648a74a7
JishvaShah <77269630+JishvaShah@users.noreply.github.com>
2024-02-05 14:45:09 -0800

    Time: 1 ms (100.00%), Space: 46.2 MB (15.91%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,29 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        int cnt=0,i=0,j=0;
-        int index=(nums1.length+nums2.length)/2;
-        int sInd=index, fInd=index-1, fVal=-1, sVal=-1;
-        
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<=nums2[j]){
-                if(fInd==cnt) fVal=nums1[i];
-                if(sInd==cnt) sVal=nums1[i];
-                cnt++;
-                i++;
-            }
-            else{
-                if(fInd==cnt) fVal=nums2[j];
-                if(sInd==cnt) sVal=nums2[j];
-                cnt++;
-                j++;
-            }
+       int n1 = nums1.length, n2 = nums2.length;
+        //if n1 is bigger swap the arrays:
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+
+        int n = n1 + n2; //total length
+        int left = (n1 + n2 + 1) / 2; //length of left half
+        //apply binary search:
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            //calculate l1, l2, r1 and r2;
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return Math.max(l1, l2);
+                else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-        while(i<nums1.length){
-            if(fInd==cnt) fVal=nums1[i];
-            if(sInd==cnt) sVal=nums1[i];
-            cnt++;
-            i++;
-        }
-        while(j<nums2.length){
-            if(fInd==cnt) fVal=nums2[j];
-            if(sInd==cnt) sVal=nums2[j];
-            cnt++;
-            j++;
-        }
-        
-        double median = 0;
-        //even length
-        if((nums1.length+nums2.length)%2==0) median=(fVal+sVal)/2.0;
-        //odd length
-        else median = (double) sVal;
-        
-        return median;
+        return 0; //dummy statement
+
     }
 }
