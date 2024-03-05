71d4bed46fffa51a9d4ae81da61d475e24f8c4b7
JishvaShah <77269630+JishvaShah@users.noreply.github.com>
2024-03-05 14:51:32 -0800

    Time: 1 ms (78.14%), Space: 41.7 MB (38.57%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,12 +5,10 @@ class Solution {
         
         String s1=strs[0], s2=strs[strs.length-1];
         for(int i=0;i<Math.min(s1.length(), s2.length());i++){
-            if(s1.charAt(i)==s2.charAt(i)){
-                sb.append(s1.charAt(i));
+            if(s1.charAt(i)!=s2.charAt(i)){
+                 return sb.toString();
             }
-            else{
-                return sb.toString();
-            }
+            sb.append(s1.charAt(i));
         }   
         return sb.toString();
     }
