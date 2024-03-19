d6ec7f37cd6fe2bd7fb4815e8d0994881ddb22f8
JishvaShah <77269630+JishvaShah@users.noreply.github.com>
2024-03-19 06:04:18 -0700

    Time: 712 ms (8.66%), Space: 45 MB (13.88%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,24 +1,22 @@
 class Solution {
     public int beautySum(String s) {
-         int ans = 0;
-        for(int i = 0; i < s.length(); i++){
-            int min = Integer.MAX_VALUE;
-            int max = 0;
-            HashMap<Character, Integer> hm = new HashMap<>();
-            for(int j = i; j < s.length(); j++){
-                if(!hm.containsKey(s.charAt(j))){
-                    hm.put(s.charAt(j), 1);
-                    max = Math.max(max, 1);
-                    min = Math.min(min, 1);
+        int mf=0,lf=0,count=0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            hm=new HashMap<>();
+            for(int j=i;j<s.length();j++){
+            hm.put(s.charAt(j),hm.getOrDefault(s.charAt(j), 0)+1);
+                if(hm.size()==1){
+                    mf=hm.get(s.charAt(j));
+                    lf=mf;
                 }
-                else{
-                    hm.put(s.charAt(j), hm.get(s.charAt(j))+1);
-                    max = Math.max(max, hm.get(s.charAt(j)));
-                    min = Collections.min(hm.values());
+                else{                 
+                    mf=(hm.get(s.charAt(j))>mf)?hm.get(s.charAt(j)):mf;
+                    lf=Collections.min(hm.values());  
                 }
-                ans = ans + (max-min);
+                count+=mf-lf;   
             }
         }
-        return ans;
+        return count;
     }
 }
